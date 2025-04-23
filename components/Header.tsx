04fdc341f67360812b02c945bec1d03aad80e95f
Andrew Cho <cho.yoonho023@gmail.com>
2025-04-23 14:02:28 -0400

    Initial Commit

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center h-20">
+            <h2 className="text-4xl font-semibold items-center p-4">OAuth mp-6</h2>
+        </header>
+    );
+}

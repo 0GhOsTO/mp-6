45d9e2f72520e043f46872d371b1dc33784c11ba
Andrew Cho <cho.yoonho023@gmail.com>
2025-04-26 19:28:56 -0400

    Major update.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,9 @@
 import {handlers} from "@/auth";
 export const{GET, POST} = handlers;
 export const runtime="edge";
+export const authOptions = {
+    session: {
+        strategy: "jwt",
+        maxAge: 0,
+    },
+};
